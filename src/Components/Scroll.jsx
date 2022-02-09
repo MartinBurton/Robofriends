297722b43acf006b397728a2886fcabea466433b
Martin Burton <martin_burton@hotmail.com>
2022-02-09 12:42:32 +0000

    Adjusted height property to give hint of scroll.

--- src/Components/Scroll.jsx
+++ src/Components/Scroll.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', height: '355px'}}>
+        <div style={{overflowY: 'scroll', height: '370px'}}>
             {props.children}
         </div>
     );
