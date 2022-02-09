0fc4a9f09cd4b30259a4fd8477894e2d89d613a9
Martin Burton <martin_burton@hotmail.com>
2022-02-09 08:57:23 +0000

    Created wrapper component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBox from './SearchBox';
 import CardList from "./CardList";
+import Scroll from './Scroll.js';
 import './App.css';
 
 class App extends React.Component {
@@ -27,15 +28,20 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList  robots={filteredRobots}/>
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList  robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        }
     }
-
 }
 
 export default App;
