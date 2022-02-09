a23674ddb45fbbfccfccab42c1e86188e3688ab3
Martin Burton <martin_burton@hotmail.com>
2022-02-09 09:13:24 +0000

    Refactored file structure, Cleanup & Build

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import SearchBox from './SearchBox';
-import CardList from "./CardList";
-import Scroll from './Scroll.js';
+import SearchBox from '../Components/SearchBox';
+import CardList from "../Components/CardList";
+import Scroll from '../Components/Scroll';
 import './App.css';
 
 class App extends React.Component {
@@ -25,13 +25,13 @@ class App extends React.Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return !robots.length ? 
+            <h1>Loading</h1> :
+            (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -40,7 +40,6 @@ class App extends React.Component {
                     </Scroll>
                 </div>
             )
-        }
     }
 }
 
