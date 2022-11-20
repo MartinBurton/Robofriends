a7c3d57fc2bf9b212acded6cd8c85cc9a697f73d
MartinBurton <martin_burton@hotmail.com>
2022-11-20 16:45:21 +0000

    Updated app to use Hooks

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,49 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBox from '../Components/SearchBox';
 import CardList from "../Components/CardList";
 import Scroll from '../Components/Scroll';
-import ErrorBoundry from "../Components/ErrorBoundry";
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        };
-    }
+function App() {
+    // React Hooks version of State
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users })
+            .then(users => setRobots(users))
+    }, []);
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
+    };
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ? 
+        <h1>Loading</h1> :
+        (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList  robots={filteredRobots}/>
+                </Scroll>
+            </div>
         )
-    }
-
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value});
-    }
-
-    render() {
-        const {robots, searchfield} = this.state;
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        });
-        return !robots.length ? 
-            <h1>Loading</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList  robots={filteredRobots}/>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            )
-    }
 }
 
 export default App;
